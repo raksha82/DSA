a6c518f4638d267d5733764e4bb5a851b263d955
raksha82 <rakshamurugesan82@gmail.com>
2026-08-11 17:23:05 +0530

    Remove Duplicates

--- src/Two_Pointerr/Remove_Duplicates.java
+++ src/Two_Pointerr/Remove_Duplicates.java
@@ -1,5 +1,36 @@
 package Two_Pointerr;
 
-public class Remove_Duplicates {
+import java.util.Arrays;
 
+public class Remove_Duplicates {
+	
+ public static void main(String[] args) {
+	 
+	 int nums[]= {0,0,1,1,1,2,2,3,3,4};
+	 int sol=Solution(nums);
+	 
+	 for(int i=sol;i<=nums.length-1;i++)
+	 {
+		 nums[i]=-1;
+	 }
+	 
+	 System.out.println(Arrays.toString(nums));
+	
+}
+ 
+ public static int Solution(int nums[])
+ {
+	 int start=1;
+	 
+	 for(int i=1;i<nums.length;i++)
+	 {
+		 if(nums[i]!=nums[start-1])
+		 {
+			 nums[start]=nums[i];
+			 start++;
+		 }
+	 }
+	 
+	 return start;
+ }
 }
