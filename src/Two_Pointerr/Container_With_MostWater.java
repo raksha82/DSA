8e28c48fc03b155a8f74425b23cf9bdf1ea22698
raksha82 <rakshamurugesan82@gmail.com>
2026-08-12 17:54:37 +0530

    Container with most water

--- src/Two_Pointerr/Container_With_MostWater.java
+++ src/Two_Pointerr/Container_With_MostWater.java
@@ -1,5 +1,37 @@
 package Two_Pointerr;
 
 public class Container_With_MostWater {
+	
+	public static void main(String[] args) {
+		
+		int height[]= {1,8,6,2,5,4,8,3,7};
+		int sol=Solution(height);
+		System.out.println("Area with Most Water:"+sol);
+	}
+	
+	static int Solution(int[] height)
+	{
+		int start=0;
+		int end=height.length-1;
+		int sum=0;
+		
+		while(start<end)
+		{
+			int area= Math.min(height[start], height[end])*(end-start);
+			sum=Math.max(sum, area);
+			
+			if(height[start]<height[end])
+			{
+				start++;
+			}
+			
+			else
+			{
+				end--;
+			}
+		}
+		
+		return sum;
+	}
 
 }
