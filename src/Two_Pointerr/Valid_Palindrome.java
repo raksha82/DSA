8a7b7199e645ed6c91fad74e71bba2e50c10840f
raksha82 <rakshamurugesan82@gmail.com>
2026-08-13 21:02:50 +0530

    Valid Palindrome

--- src/Two_Pointerr/Valid_Palindrome.java
+++ src/Two_Pointerr/Valid_Palindrome.java
@@ -1,5 +1,17 @@
 package Two_Pointerr;
 
 public class Valid_Palindrome {
+	
+	public static void main(String[] args) {
+		
+		String s="Madam";
+		String sol=Solution(s);
+	}
 
+	static String Solution(String s)
+	{
+		
+		
+		return s;
+	}
 }
