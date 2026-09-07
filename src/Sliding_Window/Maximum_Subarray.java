eff3ae24bc570497b3f89ddb106c0076f4576c99
raksha82 <rakshamurugesan82@gmail.com>
2026-09-07 16:20:26 +0530

    Sliding Window

---
+++ src/Sliding_Window/Maximum_Subarray.java
@@ -0,0 +1,33 @@
+package Sliding_Window;
+
+public class Maximum_Subarray {
+	public static void main(String[] args) {
+		
+		int[] a= {5,2,-1,0,8};
+		int k=3;
+		int result=Solution(a,k);
+		System.out.println(result);
+	}
+	
+	
+	static int Solution(int[] a, int k)
+	{
+		int currentsum=0;
+		for(int i=0;i<k;i++)
+		{
+			currentsum += a[i];
+		}
+		
+		int max=currentsum;
+		
+		for(int i=k;i<a.length;i++)
+		{
+			currentsum += a[i] - a[i-k];
+			max=Math.max(max, currentsum);
+		}
+		
+		
+		return max;
+	}
+
+}
